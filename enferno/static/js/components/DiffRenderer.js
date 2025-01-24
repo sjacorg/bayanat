35e4afc4565b141a01aa1bf908799c19ee8d9c91
Daniel Apodaca <apodacaduron@gmail.com>
2025-01-24 15:14:55 -0600

    use custom diff tool

--- enferno/static/js/components/DiffRenderer.js
+++ enferno/static/js/components/DiffRenderer.js
@@ -1,11 +1,8 @@
 const DiffRenderer = Vue.defineComponent({
     props: ['diff'],
     template: `
-    <v-sheet border color="background" class="change-item pa-2 my-1 d-flex flex-column ga-4 overflow-auto" max-height="471px">
-        <div v-for="change in diff">
-            <div class="body-2 text--black">{{ change.label }}</div>
-            <div v-html="change.diff"></div>
-        </div>
+    <v-sheet border color="background" class="change-item pa-2 my-1 d-flex flex-column ga-4 overflow-auto" max-height="471px" min-width="530px">
+        <div v-html="diff"></div>
     </v-sheet>
     `,
 });
