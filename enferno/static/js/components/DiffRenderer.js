90b13c91a103820002ad7b2b78106c6753b471c3
Daniel Apodaca <apodacaduron@gmail.com>
2025-04-22 10:54:54 -0600

    diff tool container improvements

--- enferno/static/js/components/DiffRenderer.js
+++ enferno/static/js/components/DiffRenderer.js
@@ -1,7 +1,7 @@
 const DiffRenderer = Vue.defineComponent({
     props: ['diff'],
     template: `
-    <v-sheet border color="background" class="change-item pa-2 my-1 d-flex flex-column ga-4 overflow-auto" max-height="471px" min-width="530px">
+    <v-sheet border color="background" class="change-item pa-2 my-1 d-flex flex-column ga-4 overflow-auto w-100" max-height="471px">
         <div v-html="diff"></div>
     </v-sheet>
     `,
