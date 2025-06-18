114e2080807aae174a13a96dd3773515d1d47fc3
Daniel Apodaca <apodacaduron@gmail.com>
2025-06-18 17:01:39 -0600

    initial redesign for login page

--- enferno/static/js/common/config.js
+++ enferno/static/js/common/config.js
@@ -86,6 +86,7 @@ const vuetifyConfig = {
                     rv: '#910C0A',
                     gv: '#9ECCC3',
                     pv: '#295651',
+                    background: '#FAFAFA',
                 },
             },
             dark: {
