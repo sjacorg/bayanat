0f8eb8a72b4e1f2df5c213e41dc1240eaf90968c
cango91 <can.gologlu@gmail.com>
2025-06-19 14:25:21 -0400

    Refactor DependencyUtils singleton and simplify template alerts per level09 review

--- enferno/settings.py
+++ enferno/settings.py
@@ -7,7 +7,7 @@ import redis
 from dotenv import load_dotenv, find_dotenv
 
 from enferno.utils.config_utils import ConfigManager
-from enferno.utils.dep_utils import DependencyUtils
+from enferno.utils.dep_utils import dep_utils
 
 load_dotenv(find_dotenv())
 manager = ConfigManager()
@@ -282,8 +282,8 @@ class Config(object):
     YTDLP_COOKIES = manager.get_config("YTDLP_COOKIES")
 
     # Dependency Flags
-    HAS_WHISPER = DependencyUtils.HAS_WHISPER
-    HAS_TESSERACT = DependencyUtils.HAS_TESSERACT
+    HAS_WHISPER = dep_utils.has_whisper
+    HAS_TESSERACT = dep_utils.has_tesseract
 
 
 class TestConfig(Config):
--- enferno/utils/dep_utils.py
+++ enferno/utils/dep_utils.py
@@ -1,37 +1,32 @@
 class DependencyUtils:
-    HAS_WHISPER = None
-    HAS_TESSERACT = None
+    """Simple singleton utility to check for optional dependencies."""
+
     _instance = None
 
     def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(DependencyUtils, cls).__new__(cls)
-            cls._instance.HAS_WHISPER = cls._instance.check_whisper()
-            cls._instance.HAS_TESSERACT = cls._instance.check_tesseract()
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            # Initialize checks on first creation
+            cls._instance._check_dependencies()
         return cls._instance
 
-    @classmethod
-    def check_whisper(cls):
-        if cls.HAS_WHISPER is None:
-            try:
-                import whisper
-                import torch
+    def _check_dependencies(self):
+        """Check for optional dependencies once during initialization."""
+        try:
+            import whisper
+            import torch
 
-                cls.HAS_WHISPER = True
-            except ImportError:
-                cls.HAS_WHISPER = False
-        return cls.HAS_WHISPER
+            self.has_whisper = True
+        except ImportError:
+            self.has_whisper = False
 
-    @classmethod
-    def check_tesseract(cls):
-        if cls.HAS_TESSERACT is None:
-            try:
-                import pytesseract
+        try:
+            import pytesseract
 
-                cls.HAS_TESSERACT = True
-            except ImportError:
-                cls.HAS_TESSERACT = False
-        return cls.HAS_TESSERACT
+            self.has_tesseract = True
+        except ImportError:
+            self.has_tesseract = False
 
 
-DependencyUtils()
+# Create singleton instance
+dep_utils = DependencyUtils()
