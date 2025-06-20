28fd5ab9c42fb1041e02d15c987197720cbdd80b
Daniel Apodaca <apodacaduron@gmail.com>
2025-06-20 08:54:16 -0600

    Remove unused css class

--- enferno/static/js/components/UniField.js
+++ enferno/static/js/components/UniField.js
@@ -13,7 +13,7 @@ const UniField = Vue.defineComponent({
   template: `
     <v-list v-if="english || arabic"  variant="plain" :class="['d-flex align-center flex-grow-1', { 'mx-2 my-1 pa-2': !disableSpacing }]">
       <template v-if="english && arabic">
-        <v-list-item :title="caption" density="compact" :class="{ 'p-0 pi-0': disableSpacing }">
+        <v-list-item :title="caption" density="compact" :class="{ 'px-0': disableSpacing }">
           <v-sheet class="text-body-2">{{ sw ? english : arabic }}</v-sheet>
           <template #append>
               <v-btn variant="text" size="x-small" icon="mdi-web" @click="sw= !sw"></v-btn>
@@ -22,7 +22,7 @@ const UniField = Vue.defineComponent({
       </template>
 
       <template v-else>
-        <v-list-item :title="caption" density="compact" :class="{ 'p-0 pi-0': disableSpacing }">
+        <v-list-item :title="caption" density="compact" :class="{ 'px-0': disableSpacing }">
           <v-sheet class="text-body-2">{{ english || arabic }}</v-sheet>
         </v-list-item>
       </template>
