9bd3e0adc58c1b1554979075ef244859ab66a3c9
Daniel Apodaca <apodacaduron@gmail.com>
2025-07-04 17:06:58 -0600

    centralize logic to add, remove, and search relations

--- enferno/static/js/mixins/relations-mixin.js
+++ enferno/static/js/mixins/relations-mixin.js
@@ -44,23 +44,49 @@ const relationsMixin = {
       this.isConfirmRelationDialogOpen = false;
     },
     getExcludedIds({ type, includeSelf = false, editedItem, reviewItem } = {}) {
-        const ids = [];
+      const ids = [];
 
-        const getRelations = (item) => {
-            if (!item?.id) return [];
-            const relationItems = item[`${type}_relations`] || [];
-            return relationItems.map((relation) => relation[type]?.id);
+      const getRelations = (item) => {
+        if (!item?.id) return [];
+        const relationItems = item[`${type}_relations`] || [];
+        return relationItems.map((relation) => relation[type]?.id);
+      };
+
+      if (includeSelf) {
+        if (editedItem?.id) ids.push(editedItem.id);
+        else if (reviewItem?.id) ids.push(reviewItem.id);
+      }
+
+      ids.push(...getRelations(editedItem));
+      ids.push(...getRelations(reviewItem));
+
+      return ids;
+    },
+    searchRelatedItems({ ref = {}, searchTerm = '' } = {}) {
+      ref?.open();
+      this.$nextTick(() => {
+        if ('q' in ref) ref.q = { tsv: searchTerm };
+        ref?.reSearch();
+      });
+    },
+    addItemToRelation({ type, relationList = [], item = {}, relationData = {} } = {}) {
+      this.closeConfirmRelationDialog();
+      // get list of existing attached actors
+      let ex = relationList.map((x) => x[type].id);
+
+      if (!ex.includes(item.id)) {
+        const relation = {
+          [type]: item,
+          ...relationData,
         };
+        relationList.push(relation);
+      }
+    },
 
-        if (includeSelf) {
-            if (editedItem?.id) ids.push(editedItem.id);
-            else if (reviewItem?.id) ids.push(reviewItem.id);
-        }
-
-        ids.push(...getRelations(editedItem));
-        ids.push(...getRelations(reviewItem));
-
-        return ids;
+    removeFromRelationList({ relationList = [], index } = {}) {
+      if (confirm(translations.confirm_)) {
+        relationList.splice(index, 1);
+      }
     },
   },
 };
