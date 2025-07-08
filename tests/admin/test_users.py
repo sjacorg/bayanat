fcbe707518cd26e90e1c0f838fb9d89cc68d5fb4
cango91 <can.gologlu@gmail.com>
2025-07-08 09:43:40 -0400

    fix: tests

--- tests/admin/test_users.py
+++ tests/admin/test_users.py
@@ -245,9 +245,8 @@ def test_post_password_endpoint(request, client_fixture, expected_status):
         )
         assert response.status_code == 400
         assert "password" in response.json["errors"]
-        assert (
-            "Password should be at least 8 characters long!" in response.json["errors"]["password"]
-        )
+        assert "Password should be at least " in response.json["errors"]["password"]
+        assert "characters long!" in response.json["errors"]["password"]
         response = client_.post(
             "/admin/api/password/",
             headers={"Content-Type": "application/json"},
