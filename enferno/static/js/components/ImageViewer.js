f6081aaa5a1295c57ba65c97d0de0f17b7bb84ea
Daniel Apodaca <47479471+apodacaduron@users.noreply.github.com>
2025-07-15 05:30:17 -0500

    feat: new short Actor form & Bulletin dialog redesign (#109)
    This PR brings a complete overhaul of the Bulletin dialog design, enabling analysts to access media simultaneously while editing other fields. It also introduces a new short form for Actor, accessible from inside the Bulletin dialog, to allow analysts to create new related Actors from data in Bulletins. It also includes big UX improvements to the Bulletin dialog.

---
+++ enferno/static/js/components/ImageViewer.js
@@ -0,0 +1,89 @@
+const ImageViewer = Vue.defineComponent({
+    props: ['media', 'mediaType'],
+    data: () => {
+      return {
+        translations: window.translations,
+        lg: {
+            inline: null,
+            fullscreen: null
+        },
+      };
+    },
+    unmounted() {
+        this.destroyInlineLightbox();
+        this.destroyFullscreenLightbox();
+    },
+    methods: {
+        requestFullscreen(nextOptions) {
+            const el = this.$refs.imageViewer;
+            if (!el) return;
+
+            const defaultOptions = {
+                plugins: [lgZoom, lgThumbnail, lgRotate],
+                download: false,
+                showZoomInOutIcons: true,
+                actualSize: false,
+                speed: 0,
+                selector: '.media-item',
+            }
+
+            const options = { ...defaultOptions, ...nextOptions }
+        
+            this.lg.fullscreen = lightGallery(el, options);
+            this.lg.fullscreen.openGallery(0); // Open the first image by default
+
+            // Add an event listener to destroy the fullscreen lightbox on close
+            this.lg.fullscreen.el.addEventListener('lgAfterClose', () => {
+                this.destroyFullscreenLightbox();
+            });
+        },
+        initInlineLightbox(nextOptions) {
+            const el = this.$refs.imageViewer;
+            if (!el) return;
+
+            const defaultOptions = {
+                plugins: [lgZoom, lgThumbnail, lgRotate],
+                download: false,
+                showZoomInOutIcons: true,
+                actualSize: false,
+                speed: 0,
+                selector: '.media-item',
+                container: el,
+                closable: false,
+            }
+
+            const options = { ...defaultOptions, ...nextOptions }
+        
+            this.lg.inline = lightGallery(el, options);
+            this.lg.inline.openGallery(0); // Open the first image by default
+        },
+        destroyInlineLightbox() {
+            this.lg.inline?.destroy();
+            this.lg.inline = null;
+        },
+        destroyFullscreenLightbox() {
+            this.lg.fullscreen?.destroy();
+            this.lg.fullscreen = null;
+        },
+    },
+    watch: {
+        media: {
+          deep: true,
+          immediate: true,
+          handler() {
+            this.$nextTick(() => {
+              this.destroyInlineLightbox();
+              this.initInlineLightbox(); // re-init with latest DOM
+            });
+          }
+        }
+    },
+    template: `
+        <div ref="imageViewer">
+            <a class="media-item h-100 block" :data-src="media.s3url">
+                <img :src="media.s3url" class="w-100 h-100 bg-black" style="object-fit: contain;"></img>
+            </a>
+        </div>
+      `,
+  });
+  
