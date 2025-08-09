sjacorg/bayanat

commit ce7328c3e86b6921aaba5303bb37f5e3706bcaa8

refactor: standardize API responses (#131)
⚠️ Introduces breaking API changes ⚠️
- Adds `json_error` and `json_ok` helper methods in `HTTPResponse`
class.
- All endpoints return data or errors with the new standardized format
- All creation endpoints updated to return 201 with the newly created
object à la A/B/I create endpoints
- API docs updated to reflect changes

---------

Co-authored-by: level09 <level09@gmail.com>
Co-authored-by: Daniel Apodaca <apodacaduron@gmail.com>
Co-authored-by: tarekio <7659394+tarekio@users.noreply.github.com>

enferno/user/models.py
@@ -14,6 +14,7 @@ from sqlalchemy.ext.mutable import Mutable
 from enferno.extensions import db, rds
 from enferno.settings import Config as cfg
 from enferno.utils.base import BaseMixin
+from enferno.utils.date_helper import DateHelper
 from enferno.utils.logging_utils import get_logger
 
 # Redis key namespace to set flag for forcing password reset
@@ -469,8 +470,8 @@ class Session(db.Model, BaseMixin):
     user = db.relationship("User", backref=db.backref("sessions", lazy=True))
 
     session_token = db.Column(db.String(255), unique=True, nullable=False)
-    last_active = db.Column(db.DateTime)
-    expires_at = db.Column(db.DateTime)
+    last_active = db.Column(db.DateTime())
+    expires_at = db.Column(db.DateTime())
     ip_address = db.Column(db.String(255))
 
     # Combined metadata field for location, browser, and operating system details
@@ -482,11 +483,11 @@ class Session(db.Model, BaseMixin):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "last_active": self.last_active,
-            "expires_at": self.expires_at,
+            "last_active": DateHelper.serialize_datetime(self.last_active),
+            "expires_at": DateHelper.serialize_datetime(self.expires_at),
             "ip_address": self.ip_address,
             "meta": self.meta,
             "is_active": self.is_active,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
+            "created_at": DateHelper.serialize_datetime(self.created_at),
+            "updated_at": DateHelper.serialize_datetime(self.updated_at),
         }

enferno/data_import/views.py
@@ -42,7 +42,7 @@ def data_import_before_request() -> Optional[Response]:
     """Function to check if user is authenticated before accessing the data import routes."""
     # only admins allowed to interact with these routes
     if not (current_user.has_role("Admin")):
-        return HTTPResponse.FORBIDDEN
+        return HTTPResponse.forbidden("Forbidden")
 
 
 @imports.route("/log/")
@@ -74,9 +74,9 @@ def api_import_get(id: t.id) -> Response:
     data_import = DataImport.query.get(id)
 
     if data_import is None:
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found("Data import not found")
     else:
-        return json.dumps(data_import.to_dict()), 200
+        return HTTPResponse.success(data=data_import.to_dict())
 
 
 @imports.post("/api/imports/")
@@ -108,7 +108,7 @@ def api_imports() -> Response:
         "total": result.total,
     }
 
-    return Response(json.dumps(response), content_type="application/json")
+    return HTTPResponse.success(data=response)
 
 
 # Data Import Backend API
@@ -122,7 +122,7 @@ def media_import() -> Response:
         - html page of the etl backend.
     """
     if not current_app.config["ETL_TOOL"]:
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found("ETL tool not found")
     return render_template("media-import.html")
 
 
@@ -135,12 +135,12 @@ def path_process() -> Response:
         not current_app.config.get("ETL_PATH_IMPORT")
         or current_app.config.get("ETL_ALLOWED_PATH") is None
     ):
-        return HTTPResponse.FORBIDDEN
+        return HTTPResponse.forbidden("Forbidden")
 
     allowed_path = Path(current_app.config.get("ETL_ALLOWED_PATH"))
 
     if not allowed_path.is_dir():
-        return "Allowed import path is not configured correctly", 417
+        return HTTPResponse.error("Allowed import path is not configured correctly", status=417)
 
     sub_path = request.json.get("path")
     if sub_path == "":
@@ -150,10 +150,10 @@ def path_process() -> Response:
         if safe_path:
             import_path = Path(safe_path)
         else:
-            return HTTPResponse.FORBIDDEN
+            return HTTPResponse.forbidden("Forbidden")
 
     if not import_path.is_dir():
-        return "Invalid path specified", 417
+        return HTTPResponse.error("Invalid path specified", status=417)
 
     recursive = request.json.get("recursive", False)
     if recursive:
@@ -166,7 +166,7 @@ def path_process() -> Response:
 
     output = [{"filename": os.path.basename(file), "path": file} for file in files]
 
-    return json.dumps(output), 200
+    return HTTPResponse.success(data=output)
 
 
 @imports.post("/media/process")
@@ -185,7 +185,7 @@ def etl_process() -> Response:
 
     process_files.delay(files=files, meta=meta, user_id=current_user.id, batch_id=batch_id)
 
-    return batch_id, 200
+    return HTTPResponse.success(data=batch_id)
 
 
 # CSV Tool
@@ -199,7 +199,7 @@ def csv_dashboard() -> Response:
         - html page of the csv backend.
     """
     if not current_app.config.get("SHEET_IMPORT"):
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found("Sheet import not found")
     return render_template("sheets-import.html")
 
 
@@ -214,7 +214,7 @@ def api_local_csv_upload() -> Response:
         # validate immediately
         allowed_extensions = current_app.config["SHEETS_ALLOWED_EXTENSIONS"]
         if not Media.validate_file_extension(f.filename, allowed_extensions):
-            return "This file type is not allowed", 415
+            return HTTPResponse.error("This file type is not allowed", status=415)
         # final file
         filename = Media.generate_file_name(f.filename)
         filepath = (import_dir / filename).as_posix()
@@ -224,10 +224,10 @@ def api_local_csv_upload() -> Response:
         etag = get_file_hash(filepath)
 
         response = {"etag": etag, "filename": filename, "original_filename": f.filename}
-        return Response(json.dumps(response), content_type="application/json"), 200
+        return HTTPResponse.success(data=response)
     except Exception as e:
         logger.error(e, exc_info=True)
-        return f"Request Failed", 417
+        return HTTPResponse.error("Request Failed", status=417)
 
 
 @imports.delete("/api/csv/upload/")
@@ -254,9 +254,9 @@ def api_csv_analyze() -> Response:
     result = SheetImport.parse_csv(filepath)
 
     if result:
-        return json.dumps(result)
+        return HTTPResponse.success(data=result)
     else:
-        return "Problem parsing sheet file", 417
+        return HTTPResponse.error("Problem parsing sheet file", status=417)
 
 
 # Excel sheet selector
@@ -270,7 +270,7 @@ def api_xls_sheet() -> Response:
     filepath = (import_dir / filename).as_posix()
     sheets = SheetImport.get_sheets(filepath)
 
-    return json.dumps(sheets)
+    return HTTPResponse.success(data=sheets)
 
 
 @imports.post("/api/xls/analyze")
@@ -287,9 +287,9 @@ def api_xls_analyze() -> Response:
     result = SheetImport.parse_excel(filepath, sheet)
 
     if result:
-        return Response(json.dumps(result, sort_keys=False), content_type="application/json"), 200
+        return HTTPResponse.success(data=result)
     else:
-        return "Problem parsing sheet file", 417
+        return HTTPResponse.error("Problem parsing sheet file", status=417)
 
 
 # Saved Searches
@@ -302,7 +302,7 @@ def api_mappings() -> Response:
         - successful json feed of mappings or error.
     """
     mappings = Mapping.query.all()
-    return json.dumps([map.to_dict() for map in mappings]), 200
+    return HTTPResponse.success(data=[map.to_dict() for map in mappings])
 
 
 @imports.post("/api/mapping/")
@@ -326,11 +326,13 @@ def api_mapping_create() -> Response:
         flag_modified(map, "data")
 
         if map.save():
-            return {"message": f"Mapping #{map.id} created successfully", "id": map.id}, 200
+            return HTTPResponse.success(
+                data={"id": map.id}, message=f"Mapping #{map.id} created successfully"
+            )
         else:
-            return "Error creating Mapping", 417
+            return HTTPResponse.error("Error creating Mapping", status=417)
     else:
-        return "Update request missing parameters data", 417
+        return HTTPResponse.error("Update request missing parameters data", status=417)
 
 
 @imports.put("/api/mapping/<int:id>")
@@ -355,14 +357,17 @@ def api_mapping_update(id: t.id) -> Response:
             map.data = data
             map.user_id = current_user.id
             if map.save():
-                return {"message": f"Mapping #{map.id} updated successfully", "id": map.id}, 200
+                return HTTPResponse.success(
+                    data={"id": map.id},
+                    message=f"Mapping #{map.id} updated successfully",
+                )
             else:
-                return "Error updating Mapping", 417
+                return HTTPResponse.error("Error updating Mapping", status=417)
         else:
-            return "Update request missing parameters data", 417
+            return HTTPResponse.error("Update request missing parameters data", status=417)
 
     else:
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found("Mapping not found")
 
 
 @imports.delete("/api/mapping/<int:id>")
@@ -374,13 +379,13 @@ def api_mapping_delete(id: t.id) -> Response:
     mapping = db.session.get(Mapping, id)
     if mapping:
         if not mapping.user_id == current_user.id:
-            return HTTPResponse.FORBIDDEN
+            return HTTPResponse.forbidden()
         if mapping.delete():
             return f"Mapping #{id} deleted successfully", 200
         else:
             return "Error deleting Mapping", 417
     else:
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found()
 
 
 @imports.post("/api/process-sheet")
@@ -440,14 +445,14 @@ def api_process_sheet() -> Response:
                 roles,
             )
 
-    return batch_id, 200
+    return HTTPResponse.success(data=batch_id)
 
 
 @imports.get("/api/whisper/models/")
 @roles_required("Admin")
 def api_whisper_models() -> Response:
     """Returns the list of whisper models."""
-    return jsonify({"models": Constants.WHISPER_MODEL_OPTS})
+    return HTTPResponse.success(data={"models": Constants.WHISPER_MODEL_OPTS})
 
 
 @imports.get("/api/whisper/languages/")
@@ -457,6 +462,6 @@ def api_whisper_languages() -> Response:
     if current_app.config["HAS_WHISPER"]:
         from whisper.tokenizer import TO_LANGUAGE_CODE
 
-        return jsonify({"languages": TO_LANGUAGE_CODE})
+        return HTTPResponse.success(data={"languages": TO_LANGUAGE_CODE})
     else:
-        return jsonify({"languages": []})
+        return HTTPResponse.success(data={"languages": []})

enferno/deduplication/views.py
@@ -78,7 +78,7 @@ def api_deduplication() -> Response:
         "total": data.total,
         "pending": pending,
     }
-    return Response(json.dumps(response), content_type="application/json"), 200
+    return HTTPResponse.success(data=response)
 
 
 @deduplication.route("/api/process", methods=["POST"])
@@ -88,7 +88,7 @@ def api_process() -> Response:
     Endpoint used to process all deduplication data.
     """
     start_dedup.delay(current_user.id)
-    return "Processing will start shortly.", 200
+    return HTTPResponse.success(message="Processing will start shortly.")
 
 
 @deduplication.route("/api/stop", methods=["POST"])
@@ -100,7 +100,7 @@ def api_process_stop() -> Response:
     # just remove the redis flag
     rds.set("dedup", 0)
 
-    return "Processing will stop shortly.", 200
+    return HTTPResponse.success(message="Processing will stop shortly.")
 
 
 @deduplication.cli.command()
@@ -200,6 +200,6 @@ def status() -> Response:
     """
     if status := rds.get("dedup"):
         response = {"status": status.decode()}
-        return Response(json.dumps(response), content_type="application/json"), 200
+        return HTTPResponse.success(data=response)
 
-    return "Background tasks are not running correctly", 503
+    return HTTPResponse.error("Background tasks are not running correctly", status=503)

enferno/export/views.py
@@ -31,7 +31,7 @@ def export_before_request() -> Optional[Response]:
     """Check user's permissions."""
     # check user's permissions
     if not (current_user.has_role("Admin") or current_user.can_export):
-        return HTTPResponse.FORBIDDEN
+        return HTTPResponse.forbidden("Forbidden")
 
 
 @export.route("/dashboard/")
@@ -70,8 +70,11 @@ def export_bulletins() -> Response:
             Export.__table__.name,
         )
 
-        return f"Export request created successfully, id:  {export_request.id} ", 200
-    return "Error creating export request", 417
+        return HTTPResponse.created(
+            message=f"Export request created successfully, id:  {export_request.id} ",
+            data={"item": export_request.to_dict()},
+        )
+    return HTTPResponse.error("Error creating export request", status=417)
 
 
 @export.post("/api/actor/export")
@@ -94,8 +97,11 @@ def export_actors() -> Response:
             export_request.to_mini(),
             Export.__table__.name,
         )
-        return f"Export request created successfully, id:  {export_request.id} ", 200
-    return "Error creating export request", 417
+        return HTTPResponse.created(
+            message=f"Export request created successfully, id:  {export_request.id} ",
+            data={"item": export_request.to_dict()},
+        )
+    return HTTPResponse.error("Error creating export request", status=417)
 
 
 @export.post("/api/incident/export")
@@ -118,8 +124,11 @@ def export_incidents() -> Response:
             export_request.to_mini(),
             Export.__table__.name,
         )
-        return f"Export request created successfully, id:  {export_request.id} ", 200
-    return "Error creating export request", 417
+        return HTTPResponse.created(
+            message=f"Export request created successfully, id:  {export_request.id} ",
+            data={"item": export_request.to_dict()},
+        )
+    return HTTPResponse.error("Error creating export request", status=417)
 
 
 @export.get("/api/export/<int:id>")
@@ -136,9 +145,9 @@ def api_export_get(id: t.id) -> Response:
     export = Export.query.get(id)
 
     if export is None:
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found("Export not found")
     else:
-        return json.dumps(export.to_dict()), 200
+        return HTTPResponse.success(data=export.to_dict(), message="Export retrieved successfully")
 
 
 @export.post("/api/exports/")
@@ -172,7 +181,7 @@ def api_exports() -> Response:
         "total": result.total,
     }
 
-    return Response(json.dumps(response), content_type="application/json")
+    return HTTPResponse.success(data=response)
 
 
 @export.put("/api/exports/status")
@@ -186,15 +195,15 @@ def change_export_status() -> Response:
     """
     action = request.json.get("action")
     if not action or action not in ["approve", "reject"]:
-        return "Please check request action", 417
+        return HTTPResponse.error("Please check request action", status=417)
     export_id = request.json.get("exportId")
 
     if not export_id:
-        return "Invalid export request id", 417
+        return HTTPResponse.error("Invalid export request id", status=417)
     export_request = Export.query.get(export_id)
 
     if not export_request:
-        return "Export request does not exist", 404
+        return HTTPResponse.not_found("Export request does not exist")
 
     if action == "approve":
         export_request = export_request.approve()
@@ -214,7 +223,9 @@ def change_export_status() -> Response:
             export_request.uuid = res.id
             export_request.save()
 
-            return "Export request approval will be processed shortly.", 200
+            return HTTPResponse.success(
+                message="Export request approval will be processed shortly."
+            )
 
     if action == "reject":
         export_request = export_request.reject()
@@ -228,7 +239,7 @@ def change_export_status() -> Response:
                 Export.__table__.name,
             )
 
-            return "Export request rejected.", 200
+            return HTTPResponse.success(message="Export request rejected.")
 
 
 @export.put("/api/exports/expiry")
@@ -245,17 +256,17 @@ def update_expiry() -> Response:
     export_request = Export.query.get(export_id)
 
     if export_request.expired:
-        return HTTPResponse.FORBIDDEN
+        return HTTPResponse.forbidden("Forbidden")
     else:
         try:
             export_request.set_expiry(new_date)
         except Exception as e:
-            return "Invalid expiry date", 417
+            return HTTPResponse.error("Invalid expiry date", status=417)
 
         if export_request.save():
-            return f"Updated Export #{export_id}", 200
+            return HTTPResponse.success(message=f"Updated Export #{export_id}")
         else:
-            return "Save failed", 417
+            return HTTPResponse.error("Save failed", status=417)
 
 
 @export.get("/api/exports/download")
@@ -276,10 +287,10 @@ def download_export_file() -> Response:
         # either admin or user is requester
         if not current_user.has_role("Admin"):
             if current_user.id != export.requester.id:
-                return HTTPResponse.FORBIDDEN
+                return HTTPResponse.forbidden("Forbidden")
 
         if not export_id or not export:
-            return HTTPResponse.NOT_FOUND
+            return HTTPResponse.not_found("Export not found")
         # check expiry
         if not export.expired:
             # Record activity
@@ -294,8 +305,8 @@ def download_export_file() -> Response:
                 f"{Path(*Export.export_dir.parts[1:])}", f"{export.file_id}.zip"
             )
         else:
-            return HTTPResponse.REQUEST_EXPIRED
+            return HTTPResponse.error("Request expired", status=410)
 
     except Exception as e:
         logger.error(f"Unable to decrypt export request uid {e}")
-        return HTTPResponse.NOT_FOUND
+        return HTTPResponse.not_found("Unable to decrypt export request uid")

enferno/setup/views.py
@@ -78,17 +78,17 @@ def create_admin() -> Any:
     admin_role = Role.query.filter(Role.name == "Admin").first()
 
     if admin_role.users.all():
-        return HTTPResponse.BAD_REQUEST
+        return HTTPResponse.error("Admin user already exists")
 
     data = request.json
     username = data.get("username")
     password = data.get("password")
 
     if not username or not password:
-        return HTTPResponse.BAD_REQUEST
+        return HTTPResponse.error("Username and password are required")
 
     if User.query.filter(User.username == username.lower()).first():
-        return HTTPResponse.BAD_REQUEST
+        return HTTPResponse.error("Username already exists")
 
     new_admin = User(username=username, password=hash_password(password), active=1, name="Admin")
     new_admin.roles.append(admin_role)
@@ -97,10 +97,13 @@ def create_admin() -> Any:
     try:
         db.session.commit()
         login_user(new_admin)
-        return {"message": "Admin user installed successfully"}, 201
+        return HTTPResponse.created(
+            message="Admin user installed successfully",
+            data={"item": new_admin.to_dict()},
+        )
     except Exception as e:
         db.session.rollback()
-        return HTTPResponse.INTERNAL_SERVER_ERROR
+        return HTTPResponse.error("Failed to create admin user", status=500)
 
 
 @bp_setup.get("/api/check-admin")
@@ -108,9 +111,9 @@ def check_admin() -> Dict[str, str]:
     """Check if an admin user exists."""
     admin_role = Role.query.filter(Role.name == "Admin").first()
     if admin_role and admin_role.users.first():
-        return {"status": "exists", "message": "Admin user already exists"}
+        return HTTPResponse.success(data={"status": "exists"}, message="Admin user already exists")
     else:
-        return {"status": "not_found", "message": "No admin user found"}
+        return HTTPResponse.success(data={"status": "not_found"}, message="No admin user found")
 
 
 @bp_setup.post("/api/import-data")
@@ -119,9 +122,9 @@ def import_data() -> Response:
     """Import default data into the database."""
     try:
         import_default_data()
-        return HTTPResponse.OK
+        return HTTPResponse.success(message="Default data imported successfully")
     except Exception as e:
-        return HTTPResponse.INTERNAL_SERVER_ERROR
+        return HTTPResponse.error("Failed to import default data", status=500)
 
 
 @bp_setup.get("/api/check-data-imported")
@@ -129,7 +132,7 @@ def check_data_imported() -> Dict[str, str]:
     """Check if default data has been imported."""
     if User.query.first() is not None:
         if not current_user.has_role("Admin"):
-            return HTTPResponse.FORBIDDEN
+            return HTTPResponse.forbidden()
 
     data_exists = (
         Eventtype.query.first() is not None
@@ -137,9 +140,14 @@ def check_data_imported() -> Dict[str, str]:
         and ClaimedViolation.query.first() is not None
     )
     if data_exists:
-        return {"status": "imported", "message": "Default data has been imported"}
+        return HTTPResponse.success(
+            data={"status": "imported"}, message="Default data has been imported"
+        )
     else:
-        return {"status": "not_imported", "message": "Default data has not been imported"}
+        return HTTPResponse.success(
+            data={"status": "not_imported"},
+            message="Default data has not been imported",
+        )
 
 
 @bp_setup.get("/api/default-config")
@@ -147,7 +155,7 @@ def get_default_config() -> Dict[str, Any]:
     """Retrieve the default configuration for specific keys."""
     if User.query.first() is not None:
         if not current_user.has_role("Admin"):
-            return HTTPResponse.FORBIDDEN
+            return HTTPResponse.forbidden()
 
     required_keys = [
         "FILESYSTEM_LOCAL",
@@ -175,7 +183,9 @@ def get_default_config() -> Dict[str, Any]:
 
     default_config = ConfigManager.DEFAULT_CONFIG
     filtered_config = {key: default_config[key] for key in required_keys if key in default_config}
-    return filtered_config
+    return HTTPResponse.success(
+        data=filtered_config, message="Default configuration retrieved successfully"
+    )
 
 
 @bp_setup.put("/api/complete-setup/")
@@ -198,6 +208,6 @@ def api_config_write(
     conf["SETUP_COMPLETE"] = True
 
     if ConfigManager.write_config(conf):
-        return "Configuration Saved Successfully", 200
+        return HTTPResponse.success(message="Configuration Saved Successfully")
     else:
-        return "Unable to Save Configuration", 417
+        return HTTPResponse.error("Unable to Save Configuration", status=417)

enferno/user/views.py
@@ -15,6 +15,8 @@ from enferno.user.forms import ExtendedLoginForm
 from enferno.user.models import User, Session
 from flask_security.signals import password_changed, user_authenticated
 
+from enferno.utils.http_response import HTTPResponse
+
 bp_user = Blueprint("users", __name__, static_folder="../static")
 
 client = WebApplicationClient(cfg.GOOGLE_CLIENT_ID)
@@ -65,7 +67,7 @@ def auth() -> Response:
         - redirects to Google's authorization endpoint, if Google Auth is enabled and configured properly.
     """
     if not cfg.GOOGLE_OAUTH_ENABLED or not cfg.GOOGLE_CLIENT_ALLOWED_DOMAIN:
-        return "Google Auth is not enabled or configured properly", 417
+        return HTTPResponse.error("Google Auth is not enabled or configured properly", status=417)
 
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
@@ -86,7 +88,7 @@ def auth_callback() -> Response:
     Open ID callback endpoint.
     """
     if not cfg.GOOGLE_OAUTH_ENABLED or not cfg.GOOGLE_CLIENT_ALLOWED_DOMAIN:
-        return "Google Auth is not enabled or configured properly", 417
+        return HTTPResponse.error("Google Auth is not enabled or configured properly", status=417)
 
     code = request.args.get("code")
     # Find out what URL to hit to get tokens that allow you to ask for
@@ -127,18 +129,20 @@ def auth_callback() -> Response:
         # picture = userinfo_response.json()["picture"]
         users_name = userinfo_response.json()["name"]
     else:
-        return "User email not available or not verified by Google.", 400
+        return HTTPResponse.error("User email not available or not verified by Google.")
 
     # check if email belongs to the allowed domain
     if not users_email.split("@")[-1] == cfg.GOOGLE_CLIENT_ALLOWED_DOMAIN:
-        return "User email rejected!", 403
+        return HTTPResponse.forbidden("User email rejected!")
 
     # secure login by restricting access to only matching users who already have an account
     # Check if the user with the provided email exists in the database
     u = User.query.filter(User.email == users_email).first()
     if u is None:
         # User with the provided email does not exist
-        return "User not found. Ask an administrator to create an account for you.", 404
+        return HTTPResponse.not_found(
+            "User not found. Ask an administrator to create an account for you."
+        )
 
     # Update the user's Google ID if it doesn't exist
     if u.google_id is None:
@@ -174,14 +178,14 @@ def save_settings() -> Response:
     user_id = current_user.id
     user = User.query.get(user_id)
     if not user:
-        return "Problem loading user", 417
+        return HTTPResponse.error("Problem loading user", status=417)
     user.settings = {"dark": dark}
     lang = json.get("language")
     user.settings["language"] = lang
     user.settings["setupCompleted"] = json.get("setupCompleted")
     flag_modified(user, "settings")
     user.save()
-    return "Settings Saved", 200
+    return HTTPResponse.success(message="Settings Saved")
 
 
 @bp_user.route("/settings/load", methods=["GET"])
@@ -193,11 +197,11 @@ def load_settings() -> Response:
     user = User.query.get(user_id)
 
     if not user:
-        return "Problem loading user ", 417
+        return HTTPResponse.error("Problem loading user ", status=417)
 
     settings = user.settings or {}
 
-    return Response(json.dumps(settings), content_type="Application/json"), 200
+    return HTTPResponse.success(data=settings)
 
 
 @password_changed.connect

commit e4353aef1cb267be9c82d154bfa5cec8613e82e9

fix: remove potential stacktrace exposure from responses (#153)
Several api endpoints were returning stacktrace info. This issue was introduced in #131.

enferno/admin/views.py
@@ -1383,7 +1383,10 @@ def api_location_admin_levels_reorder(validated_data: dict) -> Response:
     try:
         LocationAdminLevel.reorder(new_order)
     except Exception as e:
-        return HTTPResponse.error(str(e), status=500)
+        logger.error(f"Failed to reorder location admin levels: {str(e)}", exc_info=True)
+        return HTTPResponse.error(
+            "An internal error occurred while reordering location admin levels", status=500
+        )
     return HTTPResponse.success(
         message="Updated, user should regenerate full locations from system settings"
     )
@@ -4781,7 +4784,8 @@ def api_user_sessions(id: int) -> Any:
         return HTTPResponse.success(data={"items": sessions_data, "more": more})
 
     except Exception as e:
-        return HTTPResponse.error("Server error", status=500, errors=[str(e)])
+        logger.error(f"Failed to get sessions: {str(e)}", exc_info=True)
+        return HTTPResponse.error("Server error", status=500)
 
 
 @admin.delete("/api/session/logout")
@@ -4823,7 +4827,8 @@ def logout_session() -> Response:
             return HTTPResponse.not_found(f"Session {sessid} not found in Redis.")
 
     except Exception as e:
-        return HTTPResponse.error(f"Error while logging out session: {str(e)}", status=500)
+        logger.error(f"Error while logging out session: {str(e)}", exc_info=True)
+        return HTTPResponse.error("Error while logging out session", status=500)
 
 
 @admin.delete("/api/user/<int:user_id>/sessions/logout")
@@ -4858,7 +4863,8 @@ def logout_all_sessions(user_id: int) -> Any:
             if rds.exists(session_key):
                 rds.delete(session_key)
         except Exception as e:
-            errors.append(f"Failed to delete session {s.session_token}: {str(e)}")
+            logger.error(f"Failed to delete session {s.id}: {str(e)}", exc_info=True)
+            errors.append(f"Failed to delete session {s.id}")
 
     # Logout current session last if needed
     if current_session_logout_needed:

enferno/user/models.py
@@ -257,7 +257,8 @@ class User(UserMixin, db.Model, BaseMixin):
                 if rds.exists(session_key):
                     rds.delete(session_key)
             except Exception as e:
-                errors.append(f"Failed to delete session {s.session_token}: {str(e)}")
+                logger.error(f"Failed to delete session {s.id}: {str(e)}", exc_info=True)
+                errors.append(f"Failed to delete session {s.id}")
         if errors:
             logger.error("Failed to delete some sessions: %s", errors)
 