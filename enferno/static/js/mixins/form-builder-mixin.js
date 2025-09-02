58d3d989fc23fed8167037c7ea3c67ef93fb2c0f
Daniel Apodaca <apodacaduron@gmail.com>
2025-09-02 11:57:59 -0600

    tweak endpoint limit and pass entityType

--- enferno/static/js/mixins/form-builder-mixin.js
+++ enferno/static/js/mixins/form-builder-mixin.js
@@ -10,7 +10,7 @@ const formBuilderMixin = {
     async fetchDynamicFields(entityType) {
       try {
         this.formBuilder.loading = true;
-        const response = await axios.get(`/admin/api/dynamic-fields/?entity_type=${entityType}`);
+        const response = await axios.get(`/admin/api/dynamic-fields/?entity_type=${entityType}&limit=50`);
         this.formBuilder.dynamicFields = response.data.data;
         this.sortDynamicFields();
         this.formBuilder.originalFields = deepClone(this.formBuilder.dynamicFields); // deep clone
