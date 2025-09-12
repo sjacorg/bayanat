f2fbc3e8804993f53481fbf64f42602aace87655
Daniel Apodaca <apodacaduron@gmail.com>
2025-09-11 18:09:27 -0600

    fix typo and display arrow only if the 2 dates are present

--- enferno/static/js/components/GlobalMap.js
+++ enferno/static/js/components/GlobalMap.js
@@ -73,7 +73,8 @@ const GlobalMap = Vue.defineComponent({
       // Dates
       const dates = [];
       if (loc.from_date) dates.push(`<span class="chip">${this.$root.formatDate(loc.from_date, loc.from_date.includes('T00:00') ? this.$root.dateFormats.standardDate : this.$root.dateFormats.standardDatetime)}</span>`);
-      if (loc.to_date) dates.push(`<span><i class="mdi mdi-arrow-right mr-1"></i>${this.$root.formatDate(loc.to_date, loc.from_date.includes('T00:00') ? this.$root.dateFormats.standardDate : this.$root.dateFormats.standardDatetime)}</span>`);
+      if (loc.from_date && loc.to_date) dates.push(`<i class="mdi mdi-arrow-right mr-1">`);
+      if (loc.to_date) dates.push(`<span></i>${this.$root.formatDate(loc.to_date, loc.to_date.includes('T00:00') ? this.$root.dateFormats.standardDate : this.$root.dateFormats.standardDatetime)}</span>`);
 
       const estimated = loc.estimated
         ? `<i class="mdi mdi-calendar-question mr-1" title="${t.timingForThisEventIsEstimated_}"></i>`
