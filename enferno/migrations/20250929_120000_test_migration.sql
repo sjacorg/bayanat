aed7102980d33b9de58311df9e74b41308bebd25
level09 <level09@gmail.com>
2025-09-29 22:00:44 +0400

    Revert "Reapply "Add test migration for automatic update verification""
    This reverts commit ebdaef4b391da9bd4c385a6f003b3672514b04e5.

--- enferno/migrations/20250929_120000_test_migration.sql
+++
@@ -1,5 +0,0 @@
--- Test migration to verify automatic update system
--- This is a no-op migration that can be safely applied and removed
-
--- Add a comment to a table (safe operation)
-COMMENT ON TABLE actor IS 'Individuals or entities involved in incidents';
