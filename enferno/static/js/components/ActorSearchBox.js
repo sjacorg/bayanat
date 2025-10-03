6374dbef1087442b72cedffb780b8dbe768363d4
Daniel Apodaca <apodacaduron@gmail.com>
2025-10-03 13:02:22 -0500

    remove repeated code since its now centralized in form builder mixin

--- enferno/static/js/components/ActorSearchBox.js
+++ enferno/static/js/components/ActorSearchBox.js
@@ -113,7 +113,7 @@ const ActorSearchBox = Vue.defineComponent({
       } else {
         this.dyn.set(field.name, {
           name: field.name,
-          op: operator ?? this.getDefaultOperator(field),
+          op: operator ?? this.$root.getSearchOperatorFromFieldType(field),
           value: value
         })
       }
@@ -133,17 +133,6 @@ const ActorSearchBox = Vue.defineComponent({
 
       this.$emit('update:modelValue', newQ)
     }, 350),
-
-    getDefaultOperator(field) {
-      switch (field.field_type) {
-        case 'text':
-        case 'long_text': return 'contains'
-        case 'number': return 'eq'
-        case 'select': return 'all'
-        case 'datetime': return 'between'
-        default: return 'eq'
-      }
-    }
   },
 
   template: `
