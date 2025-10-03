463282a8ca247e0cd18c61338b9e95b9d6aa3554
level09 <level09@gmail.com>
2025-10-03 22:18:05 +0400

    Fix test migration to fail immediately instead of hanging

--- enferno/migrations/20251003_000001_test_rollback_fail.sql
+++ enferno/migrations/20251003_000001_test_rollback_fail.sql
@@ -1,13 +1,5 @@
 -- Test migration that will fail to verify rollback functionality
--- This migration intentionally contains an error to test automatic rollback
+-- This migration intentionally contains a syntax error
 
-BEGIN;
-
--- This will fail: trying to add NOT NULL column without default value to existing table
-ALTER TABLE "user" ADD COLUMN test_rollback_column VARCHAR(255) NOT NULL;
-
--- Record migration (will never reach here due to failure above)
-INSERT INTO migration_history (version, applied_at, success, error_message)
-VALUES ('20251003_000001_test_rollback_fail', CURRENT_TIMESTAMP, true, NULL);
-
-COMMIT;
+-- This will fail immediately: invalid SQL syntax
+SELECT * FROM nonexistent_table_that_does_not_exist;
