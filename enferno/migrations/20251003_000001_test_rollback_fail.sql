47109abee793d980fbaf8c0c8b10fb4cbdf6b5c3
level09 <level09@gmail.com>
2025-10-04 17:32:51 +0400

    Remove test migration file that intentionally contained a syntax error for rollback verification

--- enferno/migrations/20251003_000001_test_rollback_fail.sql
+++
@@ -1,5 +0,0 @@
--- Test migration that will fail to verify rollback functionality
--- This migration intentionally contains a syntax error
-
--- This will fail immediately: invalid SQL syntax
-SELECT * FROM nonexistent_table_that_does_not_exist;
