952c623dfb88597ac1cb441d7fea156c725316e8
Daniel Apodaca <apodacaduron@gmail.com>
2025-10-15 10:57:55 -0600

    wrap lines in diff dialog

--- enferno/static/js/components/ActorCard.js
+++ enferno/static/js/components/ActorCard.js
@@ -413,7 +413,7 @@ const ActorCard = Vue.defineComponent({
             v-model="diffDialog"
             max-width="770px"
         >
-          <v-card class="pa-5">
+          <v-card class="diff-dialog-content pa-5">
             <v-card-text>
               <div v-html="diffResult">
               </div>
