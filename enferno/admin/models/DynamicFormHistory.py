99c289a1dcd765c1a6679d4b5b7ea0222403c559
level09 <level09@gmail.com>
2025-10-16 22:15:23 +0500

    Remove change_summary field from DynamicFormHistory model (simplify initial version)

--- enferno/admin/models/DynamicFormHistory.py
+++ enferno/admin/models/DynamicFormHistory.py
@@ -15,7 +15,6 @@ class DynamicFormHistory(db.Model, BaseMixin):
     Each row captures:
     - entity_type: The entity being modified (bulletin, actor, incident)
     - fields_snapshot: Ordered list of active field definitions at the time of change
-    - change_summary: Concise diff of created/updated/deactivated fields
     - user_id: Who made the change
     - created_at: When the change occurred (inherited from BaseMixin)
     """
@@ -25,9 +24,6 @@ class DynamicFormHistory(db.Model, BaseMixin):
     id = db.Column(db.Integer, primary_key=True)
     entity_type = db.Column(db.String(50), nullable=False)  # bulletin, actor, incident
     fields_snapshot = db.Column(JSONB, nullable=False)  # Ordered list of field dicts
-    change_summary = db.Column(
-        JSONB, default=dict
-    )  # {created: [...], updated: [...], deactivated: [...]}
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     user = db.relationship("User", backref="form_histories", foreign_keys=[user_id])
@@ -38,7 +34,6 @@ class DynamicFormHistory(db.Model, BaseMixin):
             "id": self.id,
             "entity_type": self.entity_type,
             "fields_snapshot": self.fields_snapshot,
-            "change_summary": self.change_summary,
             "created_at": DateHelper.serialize_datetime(self.created_at),
             "user": self.user.to_dict() if self.user else {},
         }
