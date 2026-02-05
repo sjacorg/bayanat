23717ccd6d323d1f20b6a592d7de510f1accc4c2
Daniel Apodaca <apodacaduron@gmail.com>
2026-02-05 11:33:42 -0600

    extract preview rendering logic from media card so we can display thumbnails of multiple types of media in other places

--- enferno/static/js/components/MediaCard.js
+++ enferno/static/js/components/MediaCard.js
@@ -1,76 +1,28 @@
-const thumbnailContent = `
-  <div @click="handleMediaClick" class="h-100">
-    <!-- Image preview -->
-    <div v-if="isHoveringPreview && (mediaType === 'video' || mediaType === 'image')" class="h-100 d-flex align-center justify-center transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal text-h2">
-      <v-icon size="48" color="white">mdi-magnify-plus</v-icon>
-    </div>
-
-    <a class="media-item h-100 block" v-if="mediaType === 'image' && s3url" :data-src="s3url">
-      <img :src="s3url" class="w-100 h-100 bg-grey-lighten-2" style="object-fit: cover;"></img>
-    </a>
-
-    <!-- Video preview -->
-    <v-img v-if="mediaType === 'video'" :src="videoThumbnail" cover class="bg-grey-lighten-2 h-100">
-      <div class="d-flex align-center justify-center fill-height">
-        <v-btn icon="mdi-play-circle" variant="text" size="x-large" :class="['custom-play-icon', thumbnailBrightness > 128 ? 'dark-play-icon' : 'light-play-icon']"></v-btn>
-      </div>
-      <div v-if="mediaType === 'video' && durationFormatted" class="d-flex justify-center text-caption position-absolute bottom-0 right-0 text-white mb-2 mr-2 px-1 rounded-sm" style="background: rgba(0, 0, 0, 0.6);">
-        {{ durationFormatted }}
-      </div>
-    </v-img>
-
-    <!-- PDF preview -->
-    <div v-else-if="mediaType === 'pdf'" :class="['d-flex justify-center bg-grey-lighten-2 h-100 overflow-hidden', { 'align-center': !pdfThumbnailUrl, 'pa-4 align-start': pdfThumbnailUrl }]">
-      <img v-if="pdfThumbnailUrl" :src="pdfThumbnailUrl" class="w-100 rounded elevation-4" />
-      <v-icon v-else size="64" color="red">mdi-file-pdf-box</v-icon>
-    </div>
-
-    <!-- Audio preview -->
-    <div v-else-if="mediaType === 'audio'" class="d-flex align-center justify-center bg-grey-lighten-2 h-100">
-      <div class="d-flex align-center justify-center fill-height position-relative">
-        <v-icon size="128" color="primary">mdi-music-box</v-icon>
-        <v-btn
-          icon="mdi-play-circle"
-          variant="text"
-          size="x-large"
-          :class="['custom-play-icon', 'dark-play-icon']"
-          class="position-absolute"
-          style="top: 50%; left: 50%; transform: translate(-50%, -50%);"
-        ></v-btn>
-      </div>
-    </div>
-
-    <!-- Other file types preview -->
-    <div v-else-if="mediaType === 'unknown'" class="d-flex align-center justify-center bg-grey-lighten-2 h-100">
-      <v-icon size="64">mdi-file-download</v-icon>
-    </div>
-  </div>
-`
 const toolbarContent = `
-    <v-toolbar density="compact" class="px-2">
-      <div class="w-100 d-flex justify-space-between align-center">
-        <div class="d-flex align-center">
-          <v-icon :icon="iconMap[mediaType]" :color="mediaType === 'pdf' ? 'red' : 'primary'"></v-icon>
-          <v-divider vertical class="mx-2"></v-divider>
-          <v-chip prepend-icon="mdi-identifier" variant="text" class="font-weight-bold">{{ media.id }}</v-chip>
-          <v-divider vertical class="mx-2"></v-divider>
-          <v-tooltip location="bottom">
-            <template v-slot:activator="{ props }">
-              <v-chip prepend-icon="mdi-tag" variant="plain" v-if="media.category" size="small" v-bind="props">
-                {{ media.category.title }}
-              </v-chip>
-            </template>
-            <span>{{ translations.category_ }}</span>
-          </v-tooltip>
-        </div>
+  <v-toolbar density="compact" class="px-2">
+    <div class="w-100 d-flex justify-space-between align-center">
+      <div class="d-flex align-center">
+        <v-icon :icon="iconMap[mediaType]" :color="mediaType === 'pdf' ? 'red' : 'primary'"></v-icon>
+        <v-divider vertical class="mx-2"></v-divider>
+        <v-chip prepend-icon="mdi-identifier" variant="text" class="font-weight-bold">{{ media.id }}</v-chip>
+        <v-divider vertical class="mx-2"></v-divider>
+        <v-tooltip location="bottom">
+          <template v-slot:activator="{ props }">
+            <v-chip prepend-icon="mdi-tag" variant="plain" v-if="media.category" size="small" v-bind="props">
+              {{ media.category.title }}
+            </v-chip>
+          </template>
+          <span>{{ translations.category_ }}</span>
+        </v-tooltip>
       </div>
+    </div>
 
-    </v-toolbar>
-    <v-divider></v-divider>
-    <v-sheet>
-      <uni-field :english="media.title || 'Untitled'" :arabic="media.title_ar || ''" class="py-0 my-0" />
-    </v-sheet>
-    <v-divider></v-divider>
+  </v-toolbar>
+  <v-divider></v-divider>
+  <v-sheet>
+    <uni-field :english="media.title || 'Untitled'" :arabic="media.title_ar || ''" class="py-0 my-0" />
+  </v-sheet>
+  <v-divider></v-divider>
 `
 
 const fileMetadata = `
@@ -139,12 +91,7 @@ const MediaCard = Vue.defineComponent({
   data() {
     return {
       s3url: '',
-      videoDuration: null,
-      videoThumbnail: null,
-      pdfThumbnailUrl: null,
       translations: window.translations,
-      thumbnailBrightness: 0,
-      pdfCanvas: null,
       iconMap: {
         image: 'mdi-image',
         video: 'mdi-video',
@@ -163,128 +110,34 @@ const MediaCard = Vue.defineComponent({
       if (['application/pdf'].includes(fileType)) return 'pdf';
       return 'unknown';
     },
-    durationFormatted() {
-      return this.videoDuration ? this.formatDuration(this.videoDuration) : null;
-    },
   },
   mounted() {
     this.init();
   },
   methods: {
-    async loadPdfJs() {
-      await loadScript('/static/js/pdf.js/pdf.min.mjs');
-      await loadScript('/static/js/pdf.js/pdf.worker.min.mjs');
-    },
     init() {
       api.get(`/admin/api/media/${this.media.filename}`)
         .then(response => {
           this.s3url = response.data.url;
           this.media.s3url = response.data.url;
-          if (this.mediaType === 'video') {
-            this.getVideoDuration();
-            this.generateVideoThumbnail();
-          } else if (this.mediaType === 'pdf') {
-            this.generatePdfThumbnail();
-          }
         })
         .catch(error => console.error('Error fetching media:', error))
         .finally(() => this.$emit('ready'));
     },
-    handleMediaClick() {
-      switch (this.mediaType) {
+    handleMediaClick(payload) {
+      // If media-thumbnail passes payload, use it; otherwise create it
+      const clickPayload = payload || { media: this.media, mediaType: this.mediaType };
+      
+      switch (clickPayload.mediaType) {
         case 'pdf':
         case 'image':
         case 'video':
         case 'audio':
-          this.$emit('media-click', { media: this.media, mediaType: this.mediaType});
+          this.$emit('media-click', clickPayload);
           break;
         default:
           this.downloadFile();
       }
-    },
-    getVideoDuration() {
-      if (this.media.duration) {
-        this.videoDuration = Number(this.media.duration)
-      } else {
-        const video = document.createElement('video');
-        video.src = this.s3url;
-        video.crossOrigin = "anonymous";
-        video.onloadedmetadata = () => {
-          this.videoDuration = video.duration;
-        };
-      }
-    },
-    async generatePdfThumbnail() {
-      try {
-        if (typeof pdfjsLib === 'undefined') await this.loadPdfJs();
-
-        const pdf = await pdfjsLib.getDocument(this.s3url).promise;
-        const page = await pdf.getPage(1);
-
-        const THUMB_WIDTH = 240;
-        const DPR = window.devicePixelRatio || 1;
-
-        // Base viewport (CSS size)
-        const baseViewport = page.getViewport({ scale: 1 });
-        const scale = THUMB_WIDTH / baseViewport.width;
-
-        // Render viewport (high DPI)
-        const renderViewport = page.getViewport({
-          scale: scale * DPR
-        });
-
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-
-        // Internal resolution
-        canvas.width = Math.floor(renderViewport.width);
-        canvas.height = Math.floor(renderViewport.height);
-
-        // Display size
-        canvas.style.borderRadius = "4px";
-        canvas.classList.add("w-100");
-
-        await page.render({
-          canvasContext: ctx,
-          viewport: renderViewport
-        }).promise;
-
-        this.pdfThumbnailUrl = canvas.toDataURL('image/png');
-      } catch (err) {
-        console.error("PDF thumbnail error:", err);
-      }
-    },
-    generateVideoThumbnail() {
-      const video = document.createElement('video');
-      video.src = this.s3url;
-      video.crossOrigin = "anonymous";
-      video.onloadeddata = () => {
-        video.currentTime = 1; // Seek to 1 second
-        video.onseeked = () => {
-          const canvas = document.createElement('canvas');
-          canvas.width = video.videoWidth;
-          canvas.height = video.videoHeight;
-          const ctx = canvas.getContext('2d');
-          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-          this.videoThumbnail = canvas.toDataURL();
-          
-          // Calculate brightness of center pixels
-          const centerX = Math.floor(canvas.width / 2);
-          const centerY = Math.floor(canvas.height / 2);
-          const imageData = ctx.getImageData(centerX - 5, centerY - 5, 10, 10);
-          let brightness = 0;
-          for (let i = 0; i < imageData.data.length; i += 4) {
-            brightness += (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-          }
-          this.thumbnailBrightness = brightness / (imageData.data.length / 4);
-        };
-      };
-    },
-    formatDuration(seconds) {
-      const hrs = Math.floor(seconds / 3600);
-      const mins = Math.floor((seconds % 3600) / 60);
-      const secs = Math.floor(seconds % 60);
-      return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
     },
     downloadFile() {
       const link = document.createElement('a');
@@ -305,9 +158,15 @@ const MediaCard = Vue.defineComponent({
     <v-card v-if="miniMode" style="width: min(200px,100%); height: fit-content;" class="border border-1 mx-2" :disabled="!s3url">
       <v-card-text class="text-center pa-0">
         <v-hover v-slot="{ isHovering: isHoveringPreview, props: previewHoverProps }">
-          <div v-bind="previewHoverProps" class="preview-container position-relative cursor-pointer"
+          <div v-bind="previewHoverProps" class="preview-container position-relative"
               style="height: 120px;">
-              ${thumbnailContent}
+            <media-thumbnail
+              :media="media"
+              :s3url="s3url"
+              :show-hover-icon="isHoveringPreview"
+              clickable
+              @click="handleMediaClick"
+            />
           </div>
         </v-hover>
       </v-card-text>
@@ -335,9 +194,15 @@ const MediaCard = Vue.defineComponent({
 
       <v-card-text class="text-center pa-0">
         <v-hover v-slot="{ isHovering: isHoveringPreview, props: previewHoverProps }">
-          <div v-bind="previewHoverProps" class="preview-container position-relative cursor-pointer"
+          <div v-bind="previewHoverProps" class="preview-container position-relative"
               style="height: 160px;">
-            ${thumbnailContent}
+            <media-thumbnail
+              :media="media"
+              :s3url="s3url"
+              :show-hover-icon="isHoveringPreview"
+              clickable
+              @click="handleMediaClick"
+            />
           </div>
         </v-hover>
       </v-card-text>
---
+++ enferno/static/js/components/MediaThumbnail.js
@@ -0,0 +1,218 @@
+const MediaThumbnail = Vue.defineComponent({
+  props: {
+    media: {
+      type: Object,
+      required: true
+    },
+    s3url: {
+      type: String,
+      default: ''
+    },
+    clickable: {
+      type: Boolean,
+      default: false
+    },
+    showHoverIcon: {
+      type: Boolean,
+      default: false
+    },
+    showPlayButton: {
+      type: Boolean,
+      default: true
+    },
+    showDuration: {
+      type: Boolean,
+      default: true
+    },
+    compact: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['click'],
+  data() {
+    return {
+      videoDuration: null,
+      videoThumbnail: null,
+      pdfThumbnailUrl: null,
+      thumbnailBrightness: 0,
+    };
+  },
+  computed: {
+    mediaType() {
+      const fileType = this.media.fileType;
+      if (['image/jpeg', 'image/png', 'image/gif'].includes(fileType)) return 'image';
+      if (['video/webm', 'video/mp4', 'video/ogg'].includes(fileType)) return 'video';
+      if (['audio/mpeg', 'audio/mp3', 'audio/wav', 'audio/ogg'].includes(fileType)) return 'audio';
+      if (['application/pdf'].includes(fileType)) return 'pdf';
+      return 'unknown';
+    },
+    durationFormatted() {
+      return this.videoDuration ? this.formatDuration(this.videoDuration) : null;
+    },
+  },
+  watch: {
+    s3url: {
+      immediate: true,
+      handler(newUrl) {
+        if (newUrl) {
+          this.initThumbnail();
+        }
+      }
+    }
+  },
+  methods: {
+    async loadPdfJs() {
+      await loadScript('/static/js/pdf.js/pdf.min.mjs');
+      await loadScript('/static/js/pdf.js/pdf.worker.min.mjs');
+    },
+    initThumbnail() {
+      if (this.mediaType === 'video') {
+        this.getVideoDuration();
+        this.generateVideoThumbnail();
+      } else if (this.mediaType === 'pdf') {
+        this.generatePdfThumbnail();
+      }
+    },
+    handleClick() {
+      if (this.clickable) {
+        this.$emit('click', { media: this.media, mediaType: this.mediaType });
+      }
+    },
+    getVideoDuration() {
+      if (this.media.duration) {
+        this.videoDuration = Number(this.media.duration);
+      } else {
+        const video = document.createElement('video');
+        video.src = this.s3url;
+        video.crossOrigin = "anonymous";
+        video.onloadedmetadata = () => {
+          this.videoDuration = video.duration;
+        };
+      }
+    },
+    async generatePdfThumbnail() {
+      try {
+        if (typeof pdfjsLib === 'undefined') await this.loadPdfJs();
+
+        const pdf = await pdfjsLib.getDocument(this.s3url).promise;
+        const page = await pdf.getPage(1);
+
+        const THUMB_WIDTH = 240;
+        const DPR = window.devicePixelRatio || 1;
+
+        // Base viewport (CSS size)
+        const baseViewport = page.getViewport({ scale: 1 });
+        const scale = THUMB_WIDTH / baseViewport.width;
+
+        // Render viewport (high DPI)
+        const renderViewport = page.getViewport({
+          scale: scale * DPR
+        });
+
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        // Internal resolution
+        canvas.width = Math.floor(renderViewport.width);
+        canvas.height = Math.floor(renderViewport.height);
+
+        // Display size
+        canvas.style.borderRadius = "4px";
+        canvas.classList.add("w-100");
+
+        await page.render({
+          canvasContext: ctx,
+          viewport: renderViewport
+        }).promise;
+
+        this.pdfThumbnailUrl = canvas.toDataURL('image/png');
+      } catch (err) {
+        console.error("PDF thumbnail error:", err);
+      }
+    },
+    generateVideoThumbnail() {
+      const video = document.createElement('video');
+      video.src = this.s3url;
+      video.crossOrigin = "anonymous";
+      video.onloadeddata = () => {
+        video.currentTime = 1; // Seek to 1 second
+        video.onseeked = () => {
+          const canvas = document.createElement('canvas');
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+          this.videoThumbnail = canvas.toDataURL();
+          
+          // Calculate brightness of center pixels
+          const centerX = Math.floor(canvas.width / 2);
+          const centerY = Math.floor(canvas.height / 2);
+          const imageData = ctx.getImageData(centerX - 5, centerY - 5, 10, 10);
+          let brightness = 0;
+          for (let i = 0; i < imageData.data.length; i += 4) {
+            brightness += (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
+          }
+          this.thumbnailBrightness = brightness / (imageData.data.length / 4);
+        };
+      };
+    },
+    formatDuration(seconds) {
+      const hrs = Math.floor(seconds / 3600);
+      const mins = Math.floor((seconds % 3600) / 60);
+      const secs = Math.floor(seconds % 60);
+      return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+    },
+  },
+  template: /*html*/`
+    <div @click="handleClick" :class="['h-100', { 'cursor-pointer': clickable }]">
+      <!-- Hover icon overlay -->
+      <div v-if="showHoverIcon && clickable && (mediaType === 'video' || mediaType === 'image')" class="h-100 d-flex align-center justify-center transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal text-h2">
+        <v-icon size="48" color="white">mdi-magnify-plus</v-icon>
+      </div>
+
+      <!-- Image preview -->
+      <a class="media-item h-100 block" v-if="mediaType === 'image' && s3url" :data-src="s3url">
+        <img :src="s3url" class="w-100 h-100 bg-grey-lighten-2" style="object-fit: cover;"></img>
+      </a>
+
+      <!-- Video preview -->
+      <div v-else-if="mediaType === 'video'" class="h-100 position-relative bg-grey-lighten-2">
+        <img v-if="videoThumbnail" :src="videoThumbnail" class="w-100 h-100" style="object-fit: cover;">
+        <div v-if="showPlayButton" class="d-flex align-center justify-center position-absolute top-0 left-0 w-100 h-100">
+          <v-btn icon="mdi-play-circle" variant="text" :size="compact ? 'small' : 'x-large'" :class="['custom-play-icon', thumbnailBrightness > 128 ? 'dark-play-icon' : 'light-play-icon']"></v-btn>
+        </div>
+        <div v-if="showDuration && durationFormatted" class="d-flex justify-center text-caption position-absolute bottom-0 right-0 text-white mb-2 mr-2 px-1 rounded-sm" style="background: rgba(0, 0, 0, 0.6);">
+          {{ durationFormatted }}
+        </div>
+      </div>
+
+      <!-- PDF preview -->
+      <div v-else-if="mediaType === 'pdf'" :class="['d-flex justify-center bg-grey-lighten-2 h-100 overflow-hidden', { 'align-center': !pdfThumbnailUrl, 'pa-1 align-start': pdfThumbnailUrl && compact, 'pa-4 align-start': pdfThumbnailUrl && !compact }]">
+        <img v-if="pdfThumbnailUrl" :src="pdfThumbnailUrl" :class="['w-100', compact ? 'rounded-sm' : 'rounded elevation-4']" />
+        <v-icon v-else :size="compact ? '32' : '64'" color="red">mdi-file-pdf-box</v-icon>
+      </div>
+
+      <!-- Audio preview -->
+      <div v-else-if="mediaType === 'audio'" class="d-flex align-center justify-center bg-grey-lighten-2 h-100">
+        <div class="d-flex align-center justify-center fill-height position-relative">
+          <v-icon :size="compact ? '64' : '128'" color="primary">mdi-music-box</v-icon>
+          <v-btn
+            v-if="showPlayButton"
+            icon="mdi-play-circle"
+            variant="text"
+            :size="compact ? 'small' : 'x-large'"
+            :class="['custom-play-icon', 'dark-play-icon']"
+            class="position-absolute"
+            style="top: 50%; left: 50%; transform: translate(-50%, -50%);"
+          ></v-btn>
+        </div>
+      </div>
+
+      <!-- Other file types preview -->
+      <div v-else-if="mediaType === 'unknown'" class="d-flex align-center justify-center bg-grey-lighten-2 h-100">
+        <v-icon :size="compact ? '32' : '64'">mdi-file-download</v-icon>
+      </div>
+    </div>
+  `
+});
