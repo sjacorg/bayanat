bdd2fcfa9fd02ecbbe7d1cbc1f39cdd59bda86a6
Daniel Apodaca <47479471+apodacaduron@users.noreply.github.com>
2026-03-20 10:54:04 -0500

    Sync TinyMCE theme with Vuetify dark mode (#294)

--- enferno/static/js/common/config.js
+++ enferno/static/js/common/config.js
@@ -475,8 +475,17 @@ var tinyConfig = {
 
 // adjust rich text editor theme based on mode
 if (__settings__.dark) {
+    setTinyMceTheme('dark')
+}
+
+function setTinyMceTheme(theme) {
+  if (theme === 'dark') {
     tinyConfig.skin = 'oxide-dark';
     tinyConfig.content_css = 'dark';
+  } else {
+    tinyConfig.skin = 'oxide';
+    tinyConfig.content_css = 'default';
+  }
 }
 
 // helper prototype functions
