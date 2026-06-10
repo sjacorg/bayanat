a330a062af0c9c0b2bd4e530c7b536fd88a631bc
Daniel Apodaca <apodacaduron@gmail.com>
2026-06-10 10:01:09 -0600

    group redactions by same media type

--- enferno/static/js/components/MediaCard.js
+++ enferno/static/js/components/MediaCard.js
@@ -125,7 +125,11 @@ const MediaCard = Vue.defineComponent({
     miniMode: {
       type: Boolean,
       default: false,
-    }
+    },
+    redactions: {
+      type: Array,
+      default: () => [],
+    },
   },
   emits: ['media-click'],
   data() {
@@ -146,6 +150,7 @@ const MediaCard = Vue.defineComponent({
       ocrDetails: null,
       ocrLoading: false,
       expansionPanel: null,
+      showRedactions: false,
     };
   },
   computed: {
@@ -306,6 +311,53 @@ const MediaCard = Vue.defineComponent({
             </v-expansion-panel-text>
           </v-expansion-panel>
         </v-expansion-panels>
+      </template>
+
+      <template v-if="redactions.length">
+        <v-divider></v-divider>
+        <div
+          class="d-flex align-center justify-space-between px-3 py-2 cursor-pointer"
+          style="min-height: 36px;"
+          @click="showRedactions = !showRedactions"
+        >
+          <div class="d-flex align-center ga-2">
+            <v-icon icon="mdi-marker" size="14" color="warning"></v-icon>
+            <span class="text-caption font-weight-medium">{{ redactions.length }} redacted cop{{ redactions.length > 1 ? 'ies' : 'y' }}</span>
+          </div>
+          <v-icon :icon="showRedactions ? 'mdi-chevron-up' : 'mdi-chevron-down'" size="16" class="opacity-60"></v-icon>
+        </div>
+        <v-expand-transition>
+          <div v-if="showRedactions">
+            <v-divider></v-divider>
+            <div class="pa-2 bg-grey-lighten-4 d-flex flex-row flex-wrap ga-2">
+              <v-hover v-slot="{ isHovering, props: hoverProps }" v-for="copy in redactions" :key="copy.id">
+              <div
+                v-bind="hoverProps"
+                class="d-flex flex-column rounded bg-white overflow-hidden"
+                style="width: 96px; border: 1px solid rgba(0,0,0,0.08);"
+              >
+                <div class="position-relative" style="height: 64px;" @click.stop="$emit('media-click', { media: copy, mediaType: $root.getFileTypeFromMimeType(copy.fileType) })">
+                  <media-thumbnail :media="copy" :show-hover-icon="isHovering" :compact="true" clickable></media-thumbnail>
+                </div>
+                <div class="d-flex align-center px-1" style="min-height: 24px;">
+                  <v-tooltip location="top" :text="copy.title || copy.filename">
+                    <template #activator="{ props: ttProps }">
+                      <span v-bind="ttProps" class="text-truncate flex-grow-1" style="font-size: 10px; min-width: 0; opacity: 0.7;">{{ copy.title || copy.filename }}</span>
+                    </template>
+                  </v-tooltip>
+                  <v-btn
+                    v-if="typeof $root?.openRedactor === 'function'"
+                    icon="mdi-marker"
+                    variant="text"
+                    density="compact"
+                    @click.stop="$root.openRedactor(copy)"
+                  ></v-btn>
+                </div>
+              </div>
+              </v-hover>
+            </div>
+          </div>
+        </v-expand-transition>
       </template>
 
       <v-divider></v-divider>
--- enferno/static/js/components/MediaGrid.js
+++ enferno/static/js/components/MediaGrid.js
@@ -7,87 +7,55 @@ const MediaGrid = Vue.defineComponent({
     miniMode: Boolean,
   },
   emits: ['remove-media', 'media-click'],
-  data() {
-    return { showRedactions: false };
-  },
   computed: {
     primaryMedia() {
       const list = this.prioritizeVideos ? this.sortMediaByFileType(this.medias) : (this.medias || []);
       return list.filter(media => !this.isRedaction(media));
     },
-    redactionMedia() {
-      return (this.medias || []).filter(media => this.isRedaction(media));
-    },
-    visibleMedia() {
-      return this.showRedactions ? [...this.primaryMedia, ...this.redactionMedia] : this.primaryMedia;
+    redactionsBySource() {
+      const map = {};
+      for (const media of (this.medias || [])) {
+        if (!this.isRedaction(media)) continue;
+        const srcId = media.sourceMediaId;
+        if (srcId == null) continue;
+        if (!map[srcId]) map[srcId] = [];
+        map[srcId].push(media);
+      }
+      return map;
     },
   },
   methods: {
     isRedaction(media) {
-      return media?.category?.title === 'Redaction';
+      return media?.sourceMediaId != null;
     },
     mediaIndex(media) {
-      // Index into the unfiltered list so deletion stays correct regardless of sort/filter
       return this.medias.indexOf(media);
     },
     sortMediaByFileType(mediaList) {
       if (!mediaList) return [];
-      // Sort media list by fileType (video first)
-      const sortedMediaList = [...mediaList].sort((a, b) => {
-        if (a?.fileType?.includes('video')) return -1; // Video should come first
-        if (b?.fileType?.includes('video')) return 1; // Then images
-        return 0; // Leave unchanged if neither is a video
+      return [...mediaList].sort((a, b) => {
+        if (a?.fileType?.includes('video')) return -1;
+        if (b?.fileType?.includes('video')) return 1;
+        return 0;
       });
-
-      return sortedMediaList;
     },
   },
   template: /*html*/`
-      <div>
-        <v-sheet :class="horizontal ? 'd-flex ga-2 flex-row overflow-x-auto' : 'media-grid'">
-          <media-card
-            v-for="media in primaryMedia" :key="media.id || media.uuid"
-            @media-click="$emit('media-click', $event)"
-            :media="media"
-            :mini-mode="miniMode"
-            class="flex-shrink-0"
-          >
-            <template #actions v-if="enableDelete">
-              <v-btn size="small" variant="text" icon="mdi-delete-sweep" v-if="!media.main" @click="$emit('remove-media', mediaIndex(media))"  color="red"></v-btn>
-            </template>
-          </media-card>
-        </v-sheet>
-
-        <template v-if="redactionMedia.length">
-          <v-btn
-            variant="text" size="small" class="mt-2"
-            :prepend-icon="showRedactions ? 'mdi-eye-off-outline' : 'mdi-marker'"
-            @click="showRedactions = !showRedactions"
-          >
-            {{ showRedactions ? 'Hide' : 'Show' }} {{ redactionMedia.length }} redacted cop{{ redactionMedia.length > 1 ? 'ies' : 'y' }}
-          </v-btn>
-
-          <template v-if="showRedactions">
-            <v-divider class="my-2">
-              <v-chip size="x-small" color="warning" variant="tonal" prepend-icon="mdi-marker">
-                Redacted copies
-              </v-chip>
-            </v-divider>
-            <v-sheet :class="horizontal ? 'd-flex ga-2 flex-row overflow-x-auto' : 'media-grid'">
-              <media-card
-                v-for="media in redactionMedia" :key="media.id || media.uuid"
-                @media-click="$emit('media-click', $event)"
-                :media="media"
-                :mini-mode="miniMode"
-                class="flex-shrink-0"
-              >
-                <template #actions v-if="enableDelete">
-                  <v-btn size="small" variant="text" icon="mdi-delete-sweep" v-if="!media.main" @click="$emit('remove-media', mediaIndex(media))" color="red"></v-btn>
-                </template>
-              </media-card>
-            </v-sheet>
+    <div>
+      <v-sheet :class="horizontal ? 'd-flex ga-2 flex-row overflow-x-auto' : 'media-grid'">
+        <media-card
+          v-for="media in primaryMedia" :key="media.id || media.uuid"
+          @media-click="$emit('media-click', $event)"
+          :media="media"
+          :mini-mode="miniMode"
+          :redactions="redactionsBySource[media.id] || []"
+          class="flex-shrink-0"
+        >
+          <template #actions v-if="enableDelete">
+            <v-btn size="small" variant="text" icon="mdi-delete-sweep" v-if="!media.main" @click="$emit('remove-media', mediaIndex(media))" color="red"></v-btn>
           </template>
-        </template>
-      </div>
+        </media-card>
+      </v-sheet>
+    </div>
   `,
 });
