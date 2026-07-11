abd39647e5f74275647387fd41db92e3e910fe4a
level09 <level09@gmail.com>
2026-07-11 14:43:53 +0200

    refactor(export): create s3 client once outside media loop

--- enferno/tasks/exports.py
+++ enferno/tasks/exports.py
@@ -264,6 +264,14 @@ def generate_export_media(previous_result: int) -> t.id | Literal[False]:
         # UI switch disabled, but just in case...
         return False
 
+    if not cfg.FILESYSTEM_LOCAL:
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=cfg.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=cfg.AWS_SECRET_ACCESS_KEY,
+            region_name=cfg.AWS_REGION,
+        )
+
     for item in items:
         for media in item.medias:
             target_file = f"{Export.export_dir}/{export_request.file_id}/{media.media_file}"
@@ -280,12 +288,6 @@ def generate_export_media(previous_result: int) -> t.id | Literal[False]:
                     clear_failed_export(export_request)
                     return False  # to stop chain
             else:
-                s3 = boto3.client(
-                    "s3",
-                    aws_access_key_id=cfg.AWS_ACCESS_KEY_ID,
-                    aws_secret_access_key=cfg.AWS_SECRET_ACCESS_KEY,
-                    region_name=cfg.AWS_REGION,
-                )
                 try:
                     s3.download_file(cfg.S3_BUCKET, media.media_file, target_file)
                 except Exception:
