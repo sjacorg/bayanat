399a2a1569e709b16681f725d7debb0aea596474
Daniel Apodaca <apodacaduron@gmail.com>
2026-07-15 08:50:07 -0600

    Normalize multilingual display values

--- enferno/static/js/components/UniField.js
+++ enferno/static/js/components/UniField.js
@@ -1,29 +1,48 @@
 const UniField = Vue.defineComponent({
   props: {
     caption: String,
-    english: String | Number,
-    arabic: String | Number,
+    english: {
+      type: [String, Number],
+      default: '',
+    },
+    arabic: {
+      type: [String, Number],
+      default: '',
+    },
     disableSpacing: Boolean
   },
   data() {
     return {
-      sw: this.english ? true : false,
+      showEnglish: textUtils.hasText(this.english),
     };
   },
+  computed: {
+    hasEnglish() {
+      return textUtils.hasText(this.english);
+    },
+    hasArabic() {
+      return textUtils.hasText(this.arabic);
+    },
+  },
+  methods: {
+    displayText(value) {
+      return textUtils.normalizeDisplayText(value);
+    },
+  },
   template: `
-    <v-list v-if="english || arabic"  variant="plain" :class="['d-flex align-center flex-grow-1', { 'mx-2 my-1 pa-2': !disableSpacing }]">
-      <template v-if="english && arabic">
+    <v-list v-if="hasEnglish || hasArabic"  variant="plain" :class="['d-flex align-center flex-grow-1', { 'mx-2 my-1 pa-2': !disableSpacing }]">
+      <template v-if="hasEnglish && hasArabic">
         <v-list-item :title="caption" density="compact" :class="{ 'px-0': disableSpacing }">
-          <v-sheet class="text-body-2">{{ sw ? english : arabic }}</v-sheet>
+          <v-sheet class="text-body-2">{{ displayText(showEnglish ? english : arabic) }}</v-sheet>
           <template #append>
-              <v-btn variant="text" size="x-small" icon="mdi-web" @click.stop="sw= !sw"></v-btn>
+              <v-btn variant="text" size="x-small" icon="mdi-web" @click.stop="showEnglish = !showEnglish"></v-btn>
           </template>
         </v-list-item>
       </template>
 
       <template v-else>
         <v-list-item :title="caption" density="compact" :class="{ 'px-0': disableSpacing }">
-          <v-sheet class="text-body-2">{{ english || arabic }}</v-sheet>
+          <v-sheet class="text-body-2">{{ displayText(hasEnglish ? english : arabic) }}</v-sheet>
         </v-list-item>
       </template>
     </v-list>
